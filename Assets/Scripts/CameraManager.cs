cb7a2d616868969d7446b5bc7438c84e5cdcd989
Michael-Aga <MichaelAgarkov@gmail.com>
2024-02-11 08:05:12 +0200

    Added third person contolles

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,94 @@
+using Newtonsoft.Json.Bson;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    InputManager inputManager;
+
+    public Transform targetTransform; // The object the camera will follow
+    public Transform cameraPivot;     // The object the camera uses to pivot (Look up and down)
+    public Transform cameraTransform; // The transform of the actual camera object in the scene
+    public LayerMask collisionLayers; // The layers we want our camera to collide with
+    private float defualtPosition;
+    private Vector3 cameraFollowVelocity = Vector3.zero;
+    private Vector3 cameraVectorPosition;
+
+    public float cameraCollisionOffset = 0.2f; // How much the camera will jump off of objects its colliding with
+    public float minimumCollisionOffset = 0.2f;
+    public float cameraCollisionRadius = 0.2f;
+    public float cameraFollowSpeed = 0.2f;
+    public float cameraLookSpeed = 0.2f;
+    public float cameraPivotSpeed = 0.2f;
+    public float interpolateBy = 0.2f;
+
+    public float lookAngle; // Camera looking up and down
+    public float pivotAngle; // Camera looking left and right
+    public float minimumPivotAngle = -35;
+    public float maximumPivotAngle = 35;
+
+    private void Awake()
+    {
+        inputManager = FindObjectOfType<InputManager>();
+        targetTransform = FindObjectOfType<PlayerManager>().transform;
+        cameraTransform = Camera.main.transform;
+        defualtPosition = cameraTransform.localPosition.z;
+    }
+
+    public void HandleAllCameraMovement()
+    {
+        FollowTarget();
+        RotateCamera();
+        HandleCameraCollisions();
+    }
+
+    private void FollowTarget()
+    {
+        Vector3 targetPosition = Vector3.SmoothDamp(transform.position, targetTransform.position, ref cameraFollowVelocity, cameraFollowSpeed);
+
+        transform.position = targetPosition;
+    }
+
+    private void RotateCamera()
+    {
+        Vector3 rotation;
+        Quaternion targetRotation;
+
+        lookAngle = lookAngle + (inputManager.cameraInputX * cameraLookSpeed);
+        pivotAngle = pivotAngle - (inputManager.cameraInputY * cameraPivotSpeed);
+        pivotAngle = Mathf.Clamp(pivotAngle, minimumPivotAngle, maximumPivotAngle);
+
+        rotation = Vector3.zero;
+        rotation.y = lookAngle;
+        targetRotation = Quaternion.Euler(rotation);
+        transform.rotation = targetRotation;
+
+        rotation = Vector3.zero;
+        rotation.x = pivotAngle;
+        targetRotation = Quaternion.Euler(rotation);
+        cameraPivot.localRotation = targetRotation;
+    }
+
+    private void HandleCameraCollisions()
+    {
+        float targetPosition = defualtPosition;
+        RaycastHit hit;
+        Vector3 direction = cameraTransform.position - cameraPivot.position;
+        direction.Normalize();
+
+        if (Physics.SphereCast(cameraPivot.transform.position, cameraCollisionRadius, direction, out hit, Mathf.Abs(targetPosition), collisionLayers))
+        {
+            float distance = Vector3.Distance(cameraPivot.position, hit.point);
+            targetPosition =- (distance - cameraCollisionOffset);
+        }
+
+        if (Mathf.Abs(targetPosition) < minimumCollisionOffset)
+        {
+            targetPosition = targetPosition - minimumCollisionOffset;
+        }
+
+        cameraVectorPosition.z = Mathf.Lerp(cameraTransform.localPosition.z, targetPosition, interpolateBy);
+        cameraTransform.localPosition = cameraVectorPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class PlayerLocomotion : MonoBehaviour
+{
+    PlayerManager playerManager;
+    AnimatorManager animatorManager;
+    InputManager inputManager;
+
+    Vector3 moveDirection;
+    Transform cameraObject;
+    Rigidbody playerRigidBody;
+
+    [Header("Falling")]
+    public float inAirTimer;
+    public float leapingVelocity;
+    public float fallingVelocity;
+    public float rayCastHeightOffSet = 0.5f;
+    public LayerMask groundLayer;
+    public float maxDistance = 1;
+
+    [Header("Movement Flags")]
+    public bool isSprinting;
+    public bool isGrounded;
+
+    [Header("Movement Speeds")]
+    public float walkingSpeed = 1.5f;
+    public float runningSpeed = 5;
+    public float sprintingSpeed = 7;
+    public float rotationSpeed = 15;
+
+    private void Awake()
+    {
+        playerManager = GetComponent<PlayerManager>();
+        animatorManager = GetComponent<AnimatorManager>();
+        inputManager = GetComponent<InputManager>();
+        playerRigidBody = GetComponent<Rigidbody>();
+        cameraObject = Camera.main.transform;
+    }
+
+    public void HandleAllMovement()
+    {
+        HandleFallingAndLanding();
+
+        if (playerManager.isInteracting)
+        {
+            return;
+        }
+
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private void HandleMovement()
+    {
+        moveDirection = cameraObject.forward * inputManager.verticalInput; // Movement input
+        moveDirection = moveDirection + cameraObject.right * inputManager.horizontalInput;
+        moveDirection.Normalize();
+        moveDirection.y = 0;
+
+        if (isSprinting)
+        {
+            moveDirection = moveDirection * sprintingSpeed;
+        }
+        else
+        {
+            if (inputManager.moveAmount >= 0.5)
+            {
+                moveDirection = moveDirection * runningSpeed;
+            }
+            else
+            {
+                moveDirection = moveDirection * walkingSpeed;
+            }
+        }
+
+        Vector3 movementVelocity = moveDirection;
+        playerRigidBody.velocity = movementVelocity;
+    }
+
+    private void HandleRotation()
+    {
+        Vector3 targetDiraction = Vector3.zero;
+
+        targetDiraction = cameraObject.forward * inputManager.verticalInput;
+        targetDiraction = targetDiraction + cameraObject.right * inputManager.horizontalInput;
+        targetDiraction.Normalize();
+        targetDiraction.y = 0;
+
+        if(targetDiraction == Vector3.zero)
+        {
+            targetDiraction = transform.forward;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(targetDiraction);
+        Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        transform.rotation = playerRotation;
+    }
+
+    private void HandleFallingAndLanding()
+    {
+        RaycastHit hit;
+        Vector3 rayCastOrigin = transform.position;
+        rayCastOrigin.y = rayCastOrigin.y + rayCastHeightOffSet;
+
+        if (!isGrounded)
+        {
+            if (!playerManager.isInteracting)
+            {
+                animatorManager.PlayTargetAnimation("Falling", true);
+            }
+
+            inAirTimer = inAirTimer + Time.deltaTime;
+            playerRigidBody.AddForce(transform.forward * leapingVelocity);
+            playerRigidBody.AddForce(-Vector3.up * fallingVelocity * inAirTimer);
+        }
+
+        if (Physics.SphereCast(rayCastOrigin, 0.2f, -Vector3.up, out hit, maxDistance, groundLayer))
+        {
+            if(!isGrounded && !playerManager.isInteracting)
+            {
+                animatorManager.PlayTargetAnimation("Land", true);
+            }
+
+            inAirTimer = 0;
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+}
