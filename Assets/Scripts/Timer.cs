827dabea562bdd3d96295e47804456d7d58450b1
Michael-Aga <MichaelAgarkov@gmail.com>
2024-03-17 03:33:41 +0200

    Lint fixes

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -24,7 +24,7 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(timeValue > 0) 
+        if (timeValue > 0)
         {
             timeValue -= Time.deltaTime;
         }
@@ -40,12 +40,11 @@ public class Timer : MonoBehaviour
 
     private void DisablayTime(float timeToDisplay)
     {
-        if(timeToDisplay < 0)
+        if (timeToDisplay < 0)
         {
             timeToDisplay = 0;
         }
-
-        float minutes =Mathf.FloorToInt(timeToDisplay / 60);
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
 
         timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
